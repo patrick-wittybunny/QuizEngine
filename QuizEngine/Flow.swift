91b25acd69aabf41c8a092102cf30faa3a506e0d
Patrick <patrick.wittybunny@gmail.com>
2020-03-05 14:47:59 +0800

    Added loop to questions

--- QuizEngine/Flow.swift
+++ QuizEngine/Flow.swift
@@ -9,13 +9,14 @@
 import Foundation
 
 protocol Router {
-    func routeTo(question: String, answerCallback: @escaping (String) -> Void)
+    typealias AnswerCallback = (String) -> Void
+    func routeTo(question: String, answerCallback: @escaping AnswerCallback)
 }
 
 class Flow {
+    private let router: Router
+    private let questions: [String]
     
-    let router: Router
-    let questions: [String]
     init(questions: [String], router: Router) {
         self.questions = questions
         self.router = router
@@ -23,13 +24,21 @@ class Flow {
     
     func start() {
         if let firstQuestion = questions.first {
-            router.routeTo(question: firstQuestion) { [weak self] _ in
-                guard let self = self else { return }
-                let firstQuestionIndex = self.questions.firstIndex(of: firstQuestion)!
-                let nextQuestion = self.questions[firstQuestionIndex+1]
-                self.router.routeTo(question: nextQuestion) { _ in }
+            router.routeTo(question: firstQuestion, answerCallback: routeNext(to: firstQuestion))
+        }
+    }
+    
+    private func routeNext(to question: String) -> Router.AnswerCallback {
+        return { [weak self] _ in
+            guard let self = self else { return }
+            if let currentQuestionIndex = self.questions.firstIndex(of: question) {
+                if currentQuestionIndex+1 < self.questions.count {
+                    let nextQuestion = self.questions[currentQuestionIndex+1]
+                    self.router.routeTo(question: nextQuestion, answerCallback: self.routeNext(to: nextQuestion))
+                }
             }
         }
     }
     
 }
+ 
