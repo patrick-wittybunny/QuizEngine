67950ef8294459926c84bea988179f9a0f4648de
Patrick <patrick.wittybunny@gmail.com>
2020-03-06 15:54:56 +0800

    Added full integration and scoring for quiz engine

--- QuizEngineTests/GameTest.swift
+++ QuizEngineTests/GameTest.swift
@@ -12,15 +12,35 @@ import XCTest
 
 class GameTest: XCTestCase {
     
+    let router = RouterSpy()
+    var game: Game<String, String, RouterSpy>!
+    
+    override func setUp() {
+        super.setUp()
+        game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2" : "A2"])
+    }
+    
     func test_startGame_answerOneOutOfTwoCorrectly_scores1() {
-        let router = RouterSpy()
-        
-        startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2" : "A2"])
-        
         router.answerCallback("A1")
         router.answerCallback("wrong")
         
         XCTAssertEqual(router.routedResult!.score, 1)
+    }
+    
+    
+    func test_startGame_answerZeroOutOfTwoCorrectly_scores0() {
+        router.answerCallback("wrong")
+        router.answerCallback("wrong")
+        
+        XCTAssertEqual(router.routedResult!.score, 0)
+    }
+    
+    
+    func test_startGame_answerTwoOutOfTwoCorrectly_scores2() {
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+        
+        XCTAssertEqual(router.routedResult!.score, 2)
     }
     
 }
